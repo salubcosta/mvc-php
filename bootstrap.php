e77a19e889ce2f3a5aa4ad6d99308bcd64a588ac
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-03-10 23:04:39 -0300

    implementado route e carregamento de classe e metodo

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,15 @@
 <?php
 
 require __DIR__.'/vendor/autoload.php';
+$router = require __DIR__ .'/router.php';
+
+$obj = $router->handler();
+
+$controller = new $obj['class'];
+$action = $obj['action'];
+echo $controller->$action();
 
 // $controller = new CODE\Controller;
 // echo $controller->handler();
 
-echo (new App\Controllers\UsersController)->handler();
+// echo (new App\Controllers\UsersController)->handler();
