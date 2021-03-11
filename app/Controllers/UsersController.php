a2c699106c88783981d5834d21b462f91ed4bb2e
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-03-10 23:05:35 -0300

    Metodo index e create

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -8,4 +8,13 @@ class UsersController extends \CODE\Controller
     // {
     //     return self::class;
     // }
+    public function index()
+    {
+        return 'index - usuários';
+    }
+
+    public function create()
+    {
+        return 'create - usuários';
+    }
 }
