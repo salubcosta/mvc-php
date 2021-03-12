20626127a8f5cb69c89b1219e70e3bb0a4022ca4
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-03-11 23:38:17 -0300

    Criando construtor e metodo privado controllerNmae e deixando view default

--- src/Controller.php
+++ src/Controller.php
@@ -4,8 +4,28 @@ namespace CODE;
 
 class Controller
 {
-    public function handler()
+    protected $model;
+    
+    public function __construct(Model $model)
     {
-        return self::class;
+        $this->model = $model;
+    }
+
+    protected function render(array $data = [], string $view = null)
+    {
+        if(!$view){
+            $view =  $this->controllerName() . '/' . debug_backtrace()[1]['function'];
+        }
+        extract($data);
+        require __DIR__.'/../templates/'.$view.'.tpl.php';
+    }
+
+    private function controllerName()
+    {
+        $class = get_class($this);
+        $class = explode('\\', $class);
+        $class = array_pop($class);
+        $class = preg_replace('/Controller$/', '', $class);
+        return strtolower($class);
     }
 }
